941a5b321a75336e0f9735d158e2d49ac001e7c0
Christopher Aton <aton.christopher05@gmail.com>
2024-11-15 03:28:50 +0800

    Delete Api directory

--- Api/GetMenuByIdentifierInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- namespace UpSage\Menu\Api;
- 
- interface GetMenuByIdentifierInterface {
-  public function execute(string $identifier, int $storeId) : \UpSage\Menu\Api\Data\MenuInterface;
- }
